e4d3dfb169f50637971da6739ef96eb3e8925f90
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-07-24 02:13:08 +0200

    init manage apps file

---
+++ src/public/js/manage-app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function reloadApp(pmIdStr) {
+  const pmId = Number(pmIdStr);
+  console.log('reload app');
+}
+
+function restartApp(pmIdStr) {
+  const pmId = Number(pmIdStr);
+  console.log('restart app');
+}
+
+function stopApp(pmIdStr) {
+  const pmId = Number(pmIdStr);
+  console.log('stop app');
+}
+
+function deleteApp(pmIdStr) {
+  const pmId = Number(pmIdStr);
+  console.log('delete app');
+}
