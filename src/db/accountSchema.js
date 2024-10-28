b3e12edd1b97b595058bea1baf65de1d0ccebf3e
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-10-28 18:56:54 +0100

    change bcrypt to bcrypt-js

--- src/db/accountSchema.js
+++ src/db/accountSchema.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const uniqueValidator = require('mongoose-unique-validator');
 const config = require('../utils/config');
 
